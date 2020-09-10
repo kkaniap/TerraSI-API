6a75879c200d9674dd347b474a82f21470ad5717
kkaniap <xkaniax@gmail.com>
2020-09-10 14:52:57 +0200

    Initial commit

---
+++ src/main/java/com/terrasi/terrasiapi/TerrasiapiApplication.java
@@ -0,0 +1,13 @@
+package com.terrasi.terrasiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrasiapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerrasiapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/terrasi/terrasiapi/TerrasiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrasi.terrasiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrasiapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

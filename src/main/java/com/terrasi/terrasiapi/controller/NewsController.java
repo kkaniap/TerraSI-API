53957bf5b80927f6657adbe2860726c86b60c610
kkaniap <xkaniax@gmail.com>
2020-10-03 12:56:10 +0200

    finish testing for NewsController

--- src/main/java/com/terrasi/terrasiapi/controller/NewsController.java
+++ src/main/java/com/terrasi/terrasiapi/controller/NewsController.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;

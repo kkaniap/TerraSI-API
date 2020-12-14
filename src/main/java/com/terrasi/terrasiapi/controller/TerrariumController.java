b20b93aa563b5cec07a14172ab444948cc471d57
kkaniap <xkaniax@gmail.com>
2020-12-14 20:31:10 +0100

    cors

--- src/main/java/com/terrasi/terrasiapi/controller/TerrariumController.java
+++ src/main/java/com/terrasi/terrasiapi/controller/TerrariumController.java
@@ -82,7 +82,7 @@ public class TerrariumController {
             System.out.println("t1");
             RestTemplate rest = new RestTemplate();
             ResponseEntity<String> response = rest.exchange(
-                    "http://192.168.55.109:8080/kania",
+                    "http://192.168.55.109/kania",
                     HttpMethod.GET,
                     HttpEntity.EMPTY,
                     String.class);

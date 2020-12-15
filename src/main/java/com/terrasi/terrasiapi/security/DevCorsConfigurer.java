87148fa646a4fbe7e26e077348b4fca8503f053b
kkaniap <xkaniax@gmail.com>
2020-12-15 18:47:53 +0100

    cors

--- src/main/java/com/terrasi/terrasiapi/security/DevCorsConfigurer.java
+++ src/main/java/com/terrasi/terrasiapi/security/DevCorsConfigurer.java
@@ -1,19 +1,23 @@
 package com.terrasi.terrasiapi.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class DevCorsConfigurer implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .allowedOrigins("*")
-                .allowedHeaders("*");
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET","POST","PUT")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
